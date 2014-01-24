c56ef4544ac8cba714673a7291a7bc34ee9dd18b
arkaprava08 <arkaprava08@gmail.com>
2014-01-25 00:55:25 +0530

    added input microscope

--- public_html/js/javascript.js
+++ public_html/js/javascript.js
@@ -1,7 +1,24 @@
+/*carousel tweak*/
+$(document).foundation({
+    orbit: {
+        animation: 'slide', // Sets the type of animation used for transitioning between slides, can also be 'fade'
+        timer_speed: 5000, // Sets the amount of time in milliseconds before transitioning a slide
+        pause_on_hover: true, // Pauses on the current slide while hovering
+        resume_on_mouseout: true, // If pause on hover is set to true, this setting resumes playback after mousing out of slide
+        slide_number: false,
+        bullets: false,
+        animation_speed: 700
+    }
+});
+
+
+/*headerpage load*/
 $(function() {
     $("#dynamic_menu").load("headerpage.html");
 });
 
+
+/*stickynavigation code*/
 var stickyHeaderTop = $('#megamenu').offset().top;
 
 var height = $('#megamenu').height();
